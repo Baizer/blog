2224735741ba19ec767898239a01fa0cce8745d8
Danil <danil.lysakov@gmail.com>
2013-09-20 17:28:25 +0300

    Lesson 2

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #puts params[:post]
+
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
